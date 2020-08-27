e15e8a81e601b9df1c0067f371f5e652a6e1312d
mwinamijr <mwinamijr@gmail.com>
2020-08-27 20:09:58 +0300

    created history app

---
+++ history/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class PatientHistory(models.Model):
+    patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    history_of_presenting_illness = models.TextField(blank=True)
+    past_medical_history = models.TextField(blank=True,
+        null=True)
+    medication_history = models.TextField(blank=True,
+        null=True)
+    family_history = models.TextField(blank=True,
+        null=True)
+    social_history = models.TextField(blank=True,
+        null=True)
