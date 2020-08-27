8a66fbdb1212081f0e1f537e8cf5dc416df537b0
mwinamijr <mwinamijr@gmail.com>
2020-08-27 20:25:10 +0300

    created history serialize for api

--- history/api/serializers.py
+++ history/api/serializers.py
@@ -1,6 +1,8 @@
+from rest_framework import serializers
 from ..models import PatientHistory
 
 class PatientHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = PatientHistory
-        fields = ['id', 'patient', '']
+        fields = ['id', 'patient', 'history_of_presenting_illness', 'past_medical_history',
+         'medication_history', 'family_history', 'social_history']
