88b5de2f75eafd72107fc22abb60eca4e278d39f
mwinamijr <mwinamijr@gmail.com>
2020-08-30 09:48:03 +0300

    added history detail view with CRUD operations

--- history/api/urls.py
+++ history/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import HistoryListView
+from .views import HistoryListView, HistoryDetail
 
 urlpatterns = [
     path('patientHistory/', HistoryListView.as_view(), name='history'),
-    #path('patientHistory/<int:pk>/', HistoryListView.as_view()),
+    path('patientHistory/<int:pk>/', HistoryDetail.as_view(), name='history-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
