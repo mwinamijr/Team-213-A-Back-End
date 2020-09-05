2995f2e299a006ad65ef095cc58c404da10cc4cb
mwinamijr <mwinamijr@gmail.com>
2020-09-05 19:27:51 +0300

    updated history model

--- history/models.py
+++ history/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 class PatientHistory(models.Model):
     patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=25, blank=True, null=True)
     history_of_presenting_illness = models.TextField(blank=True)
     past_medical_history = models.TextField(blank=True,
         null=True)
@@ -14,4 +17,4 @@ class PatientHistory(models.Model):
         null=True)
 
     def __str__(self):
-        return self.patient
+        return f"{self.patient}"
