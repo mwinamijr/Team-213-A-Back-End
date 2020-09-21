2447b7903e10a2b0834ef17c53d275303a0a7670
mwinamijr <mwinamijr@gmail.com>
2020-09-21 18:52:41 +0300

    modified history model

--- history/models.py
+++ history/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class PatientHistory(models.Model):
-    patient = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
     address = models.CharField(max_length=25, blank=True, null=True)
